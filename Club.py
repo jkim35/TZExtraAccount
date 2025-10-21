2c1712f78d9520cc9b7cb673fbb9917f7be93f12
jkim35 <155586579+jkim35@users.noreply.github.com>
2025-10-21 14:14:49 -0400

    filtration is done + new type

--- Club.py
+++ Club.py
@@ -1,15 +1,15 @@
 # Imports
 import pandas as pd
 import pyautogui as robot
-from datetime import date
-#Current date + Formatter
-current_date = date.today()
+from datetime import date,timedelta
+#Current date
+current_date = pd.to_datetime(date.today())
 
-print("Formatted Date:", current_date)
 #Reading the data
 data=pd.read_csv("TestData.csv")
 data["Date"] = pd.to_datetime(data["Date"])
 print(data)
 
 #Filtering the data
-print(data.loc[(data.Date == current_date),["Name","Date","Time"]])
+specific_date = pd.to_datetime(date.today()+timedelta(days=2))
+print(data.loc[(data.Date <= specific_date)&(current_date<=data.Date),["Name","Date","Time","Type"]])
