0967ff5d2ac2235356672a7f88ed96b946d319cd
JacobCSS <jkim3@stu.socsd.org>
2025-11-04 20:41:43 -0500

    new things

--- Club.py
+++ Club.py
@@ -7,14 +7,14 @@ import time
 current_date = pd.to_datetime(date.today())
 
 #Reading the data
-data=pd.read_csv("TestData.csv")
+data=pd.read_csv("DataClubs - Form Responses 1.csv")
 data["Date"] = pd.to_datetime(data["Date"])
 print(data)
 
 #Filtering the data
 specific_date = pd.to_datetime(date.today()+timedelta(days=3))
-print(data.loc[(data.Date <= specific_date)&(current_date<=data.Date),["Name","Date","Time","Type"]])
-filteredData = data.loc[(data.Date <= specific_date)&(current_date<=data.Date),["Name","Date","Time","Type"]]
+print(data.loc[(data.Date <= specific_date)&(current_date<=data.Date),["Name","Date","Time","Type","Location"]])
+filteredData = data.loc[(data.Date <= specific_date)&(current_date<=data.Date),["Name","Date","Time","Type","Location"]]
 filteredData = filteredData.reset_index(drop=True)
 filteredData = filteredData.reset_index(drop=True)
 #Changing the data type
@@ -22,6 +22,7 @@ filteredData['Name'] = filteredData['Name'].astype(str)
 filteredData['Type'] = filteredData['Type'].astype(str)
 filteredData['Date'] = filteredData['Date'].astype(str)
 filteredData['Time'] = filteredData['Time'].astype(str)
+filteredData['Location'] = filteredData['Location'].astype(str)
 #While loop
 x=0
 numRows = len(filteredData)
@@ -42,6 +43,7 @@ while(x<numRows):
     timed = filteredData.iloc[x:x+1,2].astype(str)
  #   timed = timed[1:]
     type = filteredData.iloc[x:x+1,3].astype(str)
+    loc = filteredData.iloc[x:x+1,4].astype(str)
    # type = type[1:]
     print(dater)
     robot.write("t",interval=0.05)
@@ -52,5 +54,7 @@ while(x<numRows):
     robot.write(timed.to_string()[5:],interval=0.05)
     robot.write("//",interval=0.05)
     robot.write(type.to_string()[5:],interval=0.05)
+    robot.write("//",interval=0.05)
+    robot.write(loc.to_string()[5:],interval=0.05)
     x+=1
     robot.leftClick(500,600)
